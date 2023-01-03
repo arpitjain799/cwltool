513d1b909b20a996e2a90686df938b96e32dd422
GlassOfWhiskey <iacopo.c92@gmail.com>
2023-01-03 17:57:11 +0100

    Removed walrus operator for compatibility

--- cwltool/update.py
+++ cwltool/update.py
@@ -48,7 +48,8 @@ def v1_2to1_3dev1(
                         s["when"] = r["loopWhen"]
                         if "loop" in r:
                             for el in r["loop"]:
-                                if (source := el.pop("loopSource", None)) is not None:
+                                source = el.pop("loopSource", None)
+                                if source is not None:
                                     el["outputSource"] = source
                             s["loop"] = r["loop"]
                         if "outputMethod" in r:
